16e5b51ff7f5de67c8d7068c0be1ce11d79770a7
vallop03 <vallop03@ucm.es>
2025-11-05 18:27:09 +0100

    socorro

--- PROYECTO/public/js/formulario.js
+++ PROYECTO/public/js/formulario.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    const nombre = document.getElementById("nombre");
+    const recogida = document.getElementById("recogida");
+    const devolucion = document.getElementById("devolucion");
     const form = document.getElementById("reservaForm");
     form.addEventListener('submit', function (event) {
         if (!form.checkValidity()) {
@@ -14,4 +17,40 @@ document.addEventListener("DOMContentLoaded", function () {
         form.classList.remove('was-validated');
     });
 
+    
+    
 });
+
+
+function comprobarNombre(input) {
+    if (input.value.trim().length >= 3){
+        nombre.classList.add('is-valid');
+        nombre.classList.remove('is-invalid');
+    }
+    else {
+        nombre.classList.add('is-invalid');
+        nombre.classList.remove('is-valid');
+    }
+
+}
+
+function comprobarRecogida(input) {
+    const fechaSeleccionada = new Date(input.value);
+    const fechaActual = new Date();
+    fechaActual.setHours(0, 0, 0, 0);
+    if(fechaSeleccionada >= fechaActual){
+        recogida.classList.add('is-valid');
+        recogida.classList.remove('is-invalid');
+    } 
+    else {
+        recogida.classList.add('is-invalid');
+        recogida.classList.remove('is-valid');
+    }
+}
+
+function comprobarDevolucion(input) {
+    const fechaSeleccionada = new Date(input.value);
+    const fechaActual = new Date();
+    fechaActual.setHours(0, 0, 0, 0);
+
+}
