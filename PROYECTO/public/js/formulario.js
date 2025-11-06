29fe989f7a6957dca88534a01f8f24384d49239a
A-Unii <adrikarp@ucm.es>
2025-11-06 16:49:37 +0100

    practica 4 en proceso

--- PROYECTO/public/js/formulario.js
+++ PROYECTO/public/js/formulario.js
@@ -5,11 +5,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const devolucion = document.getElementById("devolucion");
     const form = document.getElementById("reservaForm");
     form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
+        const valido = comprobarNombre(nombre) && comprobarRecogida(recogida) && comprobarDevolucion(devolucion);
+        if (!comprobarNombre(nombre)) {
+            event.preventDefault();
+            event.stopPropagation();
         }
-        form.classList.add('was-validated');
+        if (!comprobarRecogida(recogida)) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        if (!comprobarDevolucion(devolucion)) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
     });
 
     document.getElementById("resetBtn").addEventListener("click", function () {
@@ -17,40 +25,58 @@ document.addEventListener("DOMContentLoaded", function () {
         form.classList.remove('was-validated');
     });
 
-    
-    
+    nombre.addEventListener("input", function () {
+        comprobarNombre(nombre);
+    });
+
+    recogida.addEventListener("input", function () {
+        comprobarRecogida(recogida);
+    });
+
+    devolucion.addEventListener("input", function () {
+        comprobarDevolucion(devolucion);
+    });
+
+    function comprobarNombre(input) {
+        const valor = input.value.trim();
+        let valido = valor.length >= 3;
+        if (valido) {
+            nombre.classList.add('is-valid');
+            nombre.classList.remove('is-invalid');
+        } else {
+            nombre.classList.add('is-invalid');
+            nombre.classList.remove('is-valid');
+        }
+
+        return valido;
+    }
+
+    function comprobarRecogida(input) {
+        const fechaSeleccionada = new Date(input.value);
+        const fechaActual = new Date();
+        fechaActual.setHours(0, 0, 0, 0);
+        if (fechaSeleccionada >= fechaActual) {
+            input.classList.add('is-valid');
+            input.classList.remove('is-invalid'); 
+        }
+        else {
+            input.classList.add('is-invalid');
+            input.classList.remove('is-valid');
+        }
+    }
+
+    function comprobarDevolucion(input) {
+        const fechaRecogida = new Date(recogida.value);
+        const fechaDevolucion = new Date(input.value);
+        if (fechaDevolucion > fechaRecogida) {
+            input.classList.add('is-valid');
+            input.classList.remove('is-invalid');
+        }
+        else {
+            input.classList.add('is-invalid');
+            input.classList.remove('is-valid');
+        }
+    }
+
+
 });
-
-
-function comprobarNombre(input) {
-    if (input.value.trim().length >= 3){
-        nombre.classList.add('is-valid');
-        nombre.classList.remove('is-invalid');
-    }
-    else {
-        nombre.classList.add('is-invalid');
-        nombre.classList.remove('is-valid');
-    }
-
-}
-
-function comprobarRecogida(input) {
-    const fechaSeleccionada = new Date(input.value);
-    const fechaActual = new Date();
-    fechaActual.setHours(0, 0, 0, 0);
-    if(fechaSeleccionada >= fechaActual){
-        recogida.classList.add('is-valid');
-        recogida.classList.remove('is-invalid');
-    } 
-    else {
-        recogida.classList.add('is-invalid');
-        recogida.classList.remove('is-valid');
-    }
-}
-
-function comprobarDevolucion(input) {
-    const fechaSeleccionada = new Date(input.value);
-    const fechaActual = new Date();
-    fechaActual.setHours(0, 0, 0, 0);
-
-}
